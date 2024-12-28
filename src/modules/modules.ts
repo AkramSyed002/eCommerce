051400af2de1400256b4dc843cf17fdfee794ff5
Imad Shah <shahimad499@gmail.com>
2024-12-28 23:22:31 +0500

    feat: add addToCart page

--- src/modules/modules.ts
+++ src/modules/modules.ts
@@ -2,3 +2,4 @@ export * from "./ManageHome/container/container";
 export * from "./ManageAbout/container/container";
 export * from "./ManageContact/container/container";
 export * from "./ManageAuth/container/container";
+export * from "./ShoppingCart/container/container";
--- src/screens/screens.ts
+++ src/screens/screens.ts
@@ -2,3 +2,4 @@ export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
 export { default as Contact } from "./Contact/Contact";
 export { default as Auth } from "./Auth/Auth";
+export { default as ShoppingCart } from "./ShoppingCart/ShoppingCart";
