83df4ef2535614baad646c77b1ffe34700e1642c
Imad Shah <shahimad499@gmail.com>
2024-12-31 10:35:38 +0500

    feat: add contact page

--- src/modules/ManageContact/container/ContactContainer/ContactContainer.tsx
+++ src/modules/ManageContact/container/ContactContainer/ContactContainer.tsx
@@ -1,11 +1,47 @@
-import { AppLayout } from '@muc/layout'
+import { COLORS } from "@muc/constants";
+import { AppLayout } from "@muc/layout";
+import { Box } from "@mui/material";
+import { NavLink, useLocation } from "react-router-dom";
+import { Grid } from "@mui/material";
+import { ContactDetail, ContactForm } from "../../components/components";
 
 const ContactContainer = () => {
+  const location = useLocation();
   return (
     <AppLayout>
-      <h1>Contact Container</h1>
-    </AppLayout>
-  )
-}
+      <Box sx={{ py: "80px" }}>
+        <Box sx={{ display: "flex", gap: "12px" }}>
+          <NavLink
+            to={"/"}
+            style={{
+              textDecoration: "none",
+              color: COLORS.dark.main,
+              fontSize: "14px",
+            }}
+          >
+            Home {location.pathname}
+          </NavLink>
+        </Box>
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "30px",
+            paddingY: "90px",
+          }}
+        >
+          <Grid item md={3} width={"100%"}>
+            <ContactDetail />
+          </Grid>
 
-export default ContactContainer
+          <Grid item md={7}>
+            <ContactForm />
+          </Grid>
+        </Grid>
+      </Box>
+    </AppLayout>
+  );
+};
+
+export default ContactContainer;
