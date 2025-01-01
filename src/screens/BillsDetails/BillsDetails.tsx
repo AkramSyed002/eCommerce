ee33e7b7cd9338ac878da8b8eb9a43d5a7b6dfd9
Imad Shah <shahimad499@gmail.com>
2025-01-01 14:42:45 +0500

    feat: add Bills details screen

---
+++ src/screens/BillsDetails/BillsDetails.tsx
@@ -0,0 +1,12 @@
+import { BillingDetailsContainer } from "@muc/modules";
+import React from "react";
+
+const BillsDetails = () => {
+  return (
+    <React.Fragment>
+      <BillingDetailsContainer />
+    </React.Fragment>
+  );
+};
+
+export default BillsDetails;
